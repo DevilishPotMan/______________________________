ddfd25b1930a6a8f701d5663e39d80b2b720320c
DistortedRock <147217088+DevilishPotMan@users.noreply.github.com>
2024-05-29 20:15:40 -0300

    Update adaawork_ID_Table.lua

--- adaawork_ID_Table.lua
+++ adaawork_ID_Table.lua
@@ -1,6 +1,6 @@
-local AAA = {
-    [3776740220] = true, -- distorted
-    [5803955234] = true, -- egg
-    [5868697553] = true, -- producer
-    [4643987431] = true  -- pop
+{
+  "3776740220": true,
+  "5803955234": true,
+  "5868697553": true,
+  "4643987431": true
 }
